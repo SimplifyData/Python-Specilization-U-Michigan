1256da528baac6d50d132aaaf3ae051da5157452
Atif Zafar <atif.zafar@sds.technology>
2015-12-26 18:54:04 -0800

    Connact to a page and returns content

---
+++ socket1.py
@@ -0,0 +1,19 @@
+import socket
+
+HOST = 'www.pythonlearn.com'
+
+PORT = 80
+
+mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+mysock.connect((HOST,PORT))
+
+mysock.send(b'GET http://www.pythonlearn.com/code/intro-short.txt HTTP/1.0\n\n')
+
+while True:
+    data = mysock.recv(512)
+    if ( len(data) < 1 ) :
+        break
+    print(data.decode("utf-8"))
+
+mysock.close()
