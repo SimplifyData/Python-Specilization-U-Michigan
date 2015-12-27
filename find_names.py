5562898e8f466d51d860f02a9a8fc146b610b920
Atif Zafar <atif.zafar@sds.technology>
2015-12-26 22:50:33 -0800

    Test Passed! name lookup in url working

--- find_names.py
+++ find_names.py
@@ -12,21 +12,54 @@ def find_names():
 
     sum = 0
     url = input("Website: ")
+    try:
+        browser_counter = int(input("How many times to browser url?: "))
+
+        if type(browser_counter) == int(browser_counter):
+            browser_counter = int(browser_counter)
+
+    except:
+
+        print("Please write an integer for the browser loops")
+
+        exit()
+
+    name_list = list()
 
 
+    for counter in range(browser_counter):
 
-    html = urllib.urlopen(url).read()
+        print(counter)
+        html = urllib.urlopen(url).read()
 
-    file = b_soup(html)
+        file = b_soup(html)
 
-    # retrieve span tags
+        # retrieve span tags
 
-    span_tags = file('a')
+        span_tags = file('a')
 
-    for tag in span_tags:
-        print (tag)
+        name_counter  = 1
+
+        for tag in span_tags:
 
 
+            #print(tag.contents[0])
+
+            #print (tag)
+
+            #print (tag.get('href', None))
+
+
+            if name_counter == 3:
+                name_list.append(tag.contents[0])
+
+                url = tag.get("href", None)
+
+                break
+
+            name_counter+=1
+
+    return name_list
 
 
 span_tags = find_names()
