a31547b6bdc02429248163bf8da15dab928322f1
Atif Zafar <atif.zafar@sds.technology>
2015-12-27 15:02:29 -0800

    xml count from a page. Test Passed!

---
+++ xml_find_comments.py
@@ -0,0 +1,44 @@
+import xml.etree.ElementTree as ET
+import urllib.request as urllib
+
+def find_content():
+    """
+    this function finds the content tag
+    :return:
+    """
+
+    try:
+        url = input("what is the website?: ")
+
+        html = urllib.urlopen(url).read()
+
+    except:
+        print("Please enter a valid website url")
+
+    tree = ET.fromstring(html)
+
+    counts = tree.findall('.//count')
+
+    #print ((counts))
+
+    count_num = 0
+
+    for count in counts:
+        #print (type(count))
+
+        count_text = count.text
+
+        count_num = int(count_text) + count_num
+
+        #print(count_num)
+
+
+        #count_num = count_num + int(count.txt)
+
+        #print (count_num)
+
+    return count_num
+
+xml_xount = find_content()
+
+print(xml_xount)
